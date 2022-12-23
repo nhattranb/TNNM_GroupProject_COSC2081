c54d23ef1b0eb01902295752a5be2edadc6bd9c1
nhattranb <s3926629@rmit.edu.vn>
2022-12-23 20:09:10 +0700

    The 2nd commit with changes regarding code presentation...

--- src/storesystem/Customer.java
+++ src/storesystem/Customer.java
@@ -1,3 +1,16 @@
+/*
+  RMIT University Vietnam
+  Course: COSC2081 Programming 1
+  Semester: 2022C
+  Assessment: Assignment 3
+  Author 1: Ong Gia Man (s3938231)
+  Author 2: Nguyen Le Thu Nhan (s3932151)
+  Author 3: Tran Minh Nhat (s3926629)
+  Author 4: Nguyen Ngoc Minh Thu (s3941327)
+  Date: 01/2023
+  Acknowledgement: Acknowledge the resources that you use here.
+*/
+
 package storesystem;
 
 public class Customer {
@@ -26,5 +39,7 @@ public class Customer {
         this.fullName = fullName;
     }
 
+    public int getAllOrder() {
 
+    }
 }
