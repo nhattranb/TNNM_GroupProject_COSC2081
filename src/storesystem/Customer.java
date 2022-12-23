47f1e4c638c739d0385ee9abccc88deb65b8b0ea
nhattranb <s3926629@rmit.edu.vn>
2022-12-23 19:50:43 +0700

    The first code commit for the project

--- src/storesystem/Customer.java
+++ src/storesystem/Customer.java
@@ -1,2 +1,30 @@
-package storesystem;public class Customer {
+package storesystem;
+
+public class Customer {
+    private String username;
+    private String fullName;
+    private String password;
+    private String phoneNumber;
+    private String address;
+    private String customerID;
+    private String customerType;
+
+    public void login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+
 }
