0317ffde05d151a5e7e9630dbbedbe5c5a618bdd
Nhan Nguyen <nguyenlethunhan@gmail.com>
2023-01-06 11:54:54 +0700

    Update more functions for the Customer.java file

--- src/storesystem/Customer.java
+++ src/storesystem/Customer.java
@@ -39,13 +39,25 @@ public class Customer {
             String email = sc.nextLine();
             String address = sc.nextLine();
             Customer regisUser = new Customer(username,password,fullName,phoneNumber,email,address);
-        } else if (isMember.equals("yes")) {
-            System.out.println("Please enter your username and password to login:");
-            String userName = sc.nextLine();
-            String pass = sc.nextLine();
-            Customer loginUser = new Customer(userName, pass);
         }
+        System.out.println("Please enter your username and password to login:");
+        String userName = sc.nextLine();
+        String pass = sc.nextLine();
+        Customer loginUser = new Customer(userName, pass);
 
+        // after logging in, let the user choose what they want to do
+        loginUser.showFunctionsMenu();
+        int chosenNum = sc.nextInt();
+        while (chosenNum != 0){   // if not exit
+            if (chosenNum == 1){
+                loginUser.updateInformation();
+            } else if (chosenNum ==2) {
+                System.out.println();
+            }
+            loginUser.showFunctionsMenu();
+            chosenNum = sc.nextInt();   //keep updating the chosen function
+        }
+        System.out.println("Goodbye!");
     }
     public Customer(String username, String password, String fullName, String phoneNumber,
                     String email, String address) throws IOException {
@@ -65,6 +77,7 @@ public class Customer {
             username = sc.nextLine();
             password = sc.nextLine();
         }
+        System.out.println(toString());
     }
 
     public void registration(String username, String password) throws IOException {
@@ -74,21 +87,23 @@ public class Customer {
 
         Scanner sc = new Scanner(System.in);
         String line;   // to contain each line
-        Scanner fileScanner = new Scanner("src/customers1.txt");  // create a file scanner to read from the Customer file
-        while (fileScanner.hasNext()){
-            line = fileScanner.nextLine();  // assign the next line as a string in 'line'
-            String[] values = line.split(",");   // store split values inside an array
-            while (username.equals(values[0])){
-                System.out.println("Username existed. Please use another username:");
-                username = sc.nextLine();
+        try (Scanner fileScanner = new Scanner(new File("src/customers.txt"))) {
+            while (fileScanner.hasNext()){
+                line = fileScanner.nextLine();  // assign the next line as a string in 'line'
+                String[] values = line.split(",");   // store split values inside an array
+                while (username.equals(values[0])){
+                    System.out.println("Username existed. Please use another username:");
+                    username = sc.nextLine();
+                }
             }
-        } fileScanner.close();
+        }
         // after meeting the conditions, initialize values
         this.username = username;
         this.password = password;
         // write into the file
-        PrintWriter pw = new PrintWriter(new FileWriter("src/customers1.txt", true));
+        PrintWriter pw = new PrintWriter(new FileWriter("src/customers.txt", true));
         pw.printf("%s,%s,%s,%s,%s,%s,%s\n",this.customerID,this.username,this.password,fullName,phoneNumber,email,address);
+        pw.flush();
         pw.close();
         System.out.println("Account registered!");
     }
@@ -98,7 +113,7 @@ public class Customer {
         // read username and password from Customer file
         Scanner sc = new Scanner(System.in);
         String line;
-        Scanner fileScanner = new Scanner(new File("src/customers1.txt"));
+        Scanner fileScanner = new Scanner(new File("src/customers.txt"));
         while (fileScanner.hasNext()){
             line = fileScanner.nextLine();  // assign the next line as a string in 'line'
             String[] values = line.split(",");   // store split values inside an array
@@ -116,38 +131,89 @@ public class Customer {
                 this.address = values[6];
                 return true;
             }
-        }
+        } fileScanner.close();
         return false;
     }
 
-    public void updateInformation(){
-        System.out.println("Which information do you want to update? (username, password," +
-                " fullName, phoneNumber, email, address)");
+    public void showFunctionsMenu(){
+        System.out.println("\nHere are some instruction for you. What do you want to do next?");
+        System.out.println("0. Exit");
+        System.out.println("1. Update your information.");
+        System.out.println("2. Check your current membership status.");
+        System.out.println("3. List all products");
+        System.out.println("4. Search for available products.");
+        System.out.println("5. Sort all products.");
+        System.out.println("6. Create a new order.");
+        System.out.println("7. Get order information.");
+        System.out.println("8. Get all orders you have made.");
+        System.out.println("Please enter a number to navigate your next move:");
+    }
+
+    public void updateInformation() throws IOException {
         Scanner sc = new Scanner(System.in);
-        String type = sc.next();    // take information type that user want to change
-        System.out.printf("Enter your new %s: \n", type);
+        System.out.println("Which information do you want to update? (1.username, 2.password," +
+                " 3.fullName, 4.phoneNumber, 5.email, 6.address)");
+        System.out.println("Please enter an appropriate number:");
+        int type = sc.nextInt();    // take information type that user want to change
+        sc.nextLine();
+        System.out.println("Enter your new information:");
         String newValue = sc.nextLine();
         switch (type){
-            case "username":
+            case 1:
                 setUsername(newValue);
                 break;
-            case "password":
+            case 2:
                 setPassword(newValue);
                 break;
-            case "fullName":
+            case 3:
                 setFullName(newValue);
                 break;
-            case "phoneNumber":
+            case 4:
                 setPhoneNumber(newValue);
                 break;
-            case "email":
+            case 5:
                 setEmail(newValue);
                 break;
-            case "address":
+            case 6:
                 setAddress(newValue);
                 break;
         }
-        System.out.printf("Your %s has been changed.\n", type);
+
+        try {
+            File oldFile = new File("src/customers.txt");
+            File tempFile = new File("src/temp.txt");   // create a temporary text file to store new data
+            PrintWriter pw = new PrintWriter(tempFile);   // write into the temporary file
+            Scanner fileScanner = new Scanner(oldFile);
+            String line;
+            while (fileScanner.hasNext()){
+                line = fileScanner.nextLine();
+                String[] values = line.split(",");
+                if (values[0].equals(customerID)){
+                    pw.printf("%s,%s,%s,%s,%s,%s,%s\n",customerID,getUsername(),getPassword(),getFullName(),getPhoneNumber(),getEmail(),getAddress());
+                } else {
+                    pw.printf("%s,%s,%s,%s,%s,%s,%s\n",values[0],values[1],values[2],values[3],values[4],values[5],values[6]);
+                }
+            }
+            pw.flush();
+            pw.close();
+            fileScanner.close();
+
+            // copy back to the customers.txt file
+            Scanner readFile = new Scanner(tempFile);
+            PrintWriter writeNewData = new PrintWriter(oldFile);
+            while (readFile.hasNext()){
+                writeNewData.println(readFile.nextLine());
+            }
+            readFile.close();
+            writeNewData.close();
+            tempFile.delete();  // delete the temporary file after use
+
+        } catch (Exception e){
+            System.out.println("Something was wrong!");
+            e.printStackTrace();    // print out the exception occur
+        } finally {
+            System.out.println("Your information has been changed.");
+        }
     }
 
 
@@ -160,15 +226,15 @@ public class Customer {
 
     @Override
     public String toString() {
-        return "The Customer information are as follow{" +
-                "username='" + username + '\'' +
-                ", fullName='" + fullName + '\'' +
-                ", password='" + password + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", address='" + address + '\'' +
-                ", customerID='" + customerID + '\'' +
-                ", customerType='" + customerType + '\'' +
-                '}';
+        return "The Customer information are as follow[" +
+                "\ncustomer ID='"+ customerID+ '\'' +
+                "\nusername='" + username + '\'' +
+                "\nfull name='" + fullName + '\'' +
+                "\npassword='" + password + '\'' +
+                "\nphone number='" + phoneNumber + '\'' +
+                "\naddress='" + address + '\'' +
+                "\ncustomerType='" + customerType + '\'' +
+                ']';
     }
 
     public void setUsername(String username) {
@@ -199,6 +265,30 @@ public class Customer {
     public void setAddress(String address){
         // setting a new address
         this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
     }
 
 }
